e6c184897a8d2d1932de1227b51edabfd0650150
GHERBI Amayas <amayas.gherbi@etu.univ-grenoble-alpes.fr>
2025-02-18 19:58:28 +0100

    update hello_world examples with TypeScript and JavaScript code, enhance index.html structure

--- hello_world/index.js
+++ hello_world/index.js
@@ -1,7 +1,25 @@
-var age = 20;
-if (age >= 18) {
-    console.log("You are an adult");
+"use strict";
+let person = 'steve';
+let hieghtInCm = 182.88;
+let names = ['awwais Mirza', 'John Doe', 'Jane Doe'];
+let numtest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let booltest = [true, false, true, false];
+let tuplTest = ['awwais', 1234, true];
+var DoorStates;
+(function (DoorStates) {
+    DoorStates[DoorStates["Open"] = 0] = "Open";
+    DoorStates[DoorStates["Closed"] = 1] = "Closed";
+    DoorStates[DoorStates["Ajar"] = 2] = "Ajar";
+})(DoorStates || (DoorStates = {}));
+let openDoor = DoorStates.Open;
+let closedDoor = DoorStates.Closed;
+let ajarDoor = DoorStates.Ajar;
+console.log(openDoor);
+function log(val) {
+    console.log(val);
 }
-else {
-    console.log("You are a minor");
-}
+let tel_jokes = (number) => {
+    console.log('why did the phone go to school?');
+    console.log('to get a little smarter');
+};
+console.log(tel_jokes(1234));
--- hello_world/index.ts
+++ hello_world/index.ts
@@ -1,6 +1,37 @@
-let age : number = 20;
-if (age >= 18) {
-    console.log("You are an adult");
-} else {
-    console.log("You are a minor");
+
+
+let person: string = 'steve';
+
+let hieghtInCm: number = 182.88;
+
+let names: String[] = [ 'awwais Mirza', 'John Doe', 'Jane Doe' ];
+
+let numtest:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+let booltest:boolean[] = [true, false, true, false];
+
+let tuplTest: [string, number,boolean] = ['awwais', 1234, true];
+
+enum DoorStates {
+    Open,
+    Closed,
+    Ajar
 }
+
+let openDoor = DoorStates.Open;
+let closedDoor = DoorStates.Closed;
+let ajarDoor = DoorStates.Ajar;
+
+console.log(openDoor);
+
+function log (val: any ){
+    console.log(val);
+
+}
+
+let tel_jokes = (number: number) => {
+    console.log('why did the phone go to school?');
+    console.log('to get a little smarter');
+}
+
+console.log(tel_jokes(1234));
